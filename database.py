cd693ab3d4ebcaef4a36beab27e8a714c3ca7d29
Ismail Akdim <1064259@hr.nl>
2023-05-29 08:36:53 +0200

    changes the way error codes work, improved db code

--- database.py
+++ database.py
@@ -1,6 +1,5 @@
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, LargeBinary
+from sqlalchemy.orm import sessionmaker, relationship, declarative_base
 import bcrypt
 
 db_url = 'mysql+mysqlconnector://root:@localhost/Bank'
@@ -11,25 +10,23 @@ Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
 
+class User(Base):
+    __tablename__ = 'users'
+
+    user_id = Column(Integer, primary_key=True)
+    pin_hash = Column(LargeBinary(60))
+    accounts = relationship("Account", back_populates="user")
+
+    def verify_pin(self, pin):
+        return bcrypt.checkpw(pin.encode('utf-8'), self.pin_hash)
 
 class Account(Base):
     __tablename__ = 'accounts'
 
     account_id = Column(Integer, primary_key=True)
-    user_id = Column(Integer)
+    user_id = Column(Integer, ForeignKey('users.user_id'))
     iban = Column(String(16))
     balance = Column(Integer)
     status = Column(Integer)
     attempts = Column(Integer)
-
-
-class User(Base):
-    __tablename__ = 'users'
-
-    user_id = Column(Integer, primary_key=True)
-    pin_hash = Column(String(255))
-
-    def verify_pin(self, pin):
-        account = session.query(Account).filter_by(user_id=self.user_id).first()
-        user = session.query(User).filter_by(user_id=account.user_id).first()
-        return bcrypt.checkpw(pin.encode('utf-8'), user.pin_hash.encode('utf-8'))
+    user = relationship("User", back_populates="accounts")
--- main.py
+++ main.py
@@ -6,34 +6,23 @@ app = Flask(__name__)
 @app.route('/api/balance', methods=['POST'])
 def balance():
     data = request.get_json()
-    account_id = data.get('acctNo')
+    iban = data.get('acctNo')
     pin = data.get('pin')
-
     try:
-        account = session.query(Account).filter_by(account_id=account_id).first()
-
+        account = session.query(Account).filter_by(iban=iban).first()
         if account:
             user = session.query(User).filter_by(user_id=account.user_id).first()
-
             if user and user.verify_pin(pin):
                 balance = account.balance
-                response = {'status': 'success', 'balance': balance}
-                # Change the response code to 201 (Created)
-                return make_response(jsonify(response), 201)
+                response = {'status': 200, 'balance': balance}
             else:
-                response = {'status': 'error', 'message': 'Invalid user ID or PIN.'}
-                # Change the response code to 400 (Bad Request)
-                return make_response(jsonify(response), 400)
+                response = {'status': 400, 'message': 'Invalid user ID or PIN.'}
         else:
-            response = {'status': 'error', 'message': 'Invalid account number.'}
-            # Change the response code to 400 (Bad Request)
-            return make_response(jsonify(response), 400)
-
+            response = {'status': 400, 'message': 'Invalid account number.'}
     except Exception as e:
         print(f"Database Error: {str(e)}")
-        response = {'status': 'error', 'message': 'An error occurred while accessing the database.'}
-        # Change the response code to 500 (Internal Server Error)
-        return make_response(jsonify(response), 500)
+        response = {'status': 400, 'message': 'An error occurred while accessing the database.'}
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
