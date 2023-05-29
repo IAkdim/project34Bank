1fea1e6c496c65b210fd7c47e763a2002c597bec
Ismail Akdim <1064259@hr.nl>
2023-05-29 20:15:14 +0200

    added keypad code

---
+++ keypad/main.py
@@ -0,0 +1,28 @@
+import serial
+import time
+from pynput.keyboard import Controller, Key
+
+def process_serial_data(serial_port, baud_rate=9600):
+    ser = serial.Serial(serial_port, baud_rate)
+    keyboard = Controller()
+
+    while True:
+        line = ser.readline().decode().strip()
+        if line.startswith("<") and line.endswith(">"):
+            key = line[1:-1]
+            if key == 'A':
+                keyboard.press(Key.enter)
+                keyboard.release(Key.enter)
+            elif key == 'B':
+                keyboard.press(Key.backspace)
+                keyboard.release(Key.backspace)
+            elif key == 'D':
+                keyboard.press(Key.tab)
+                keyboard.release(Key.tab)
+            else:
+                keyboard.press(key)
+                keyboard.release(key)
+        time.sleep(0.1)  # Adjust the delay if needed
+
+if __name__ == '__main__':
+    process_serial_data()
