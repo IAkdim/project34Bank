11345defc8c4fff798828137e67a721bba9adf78
Ismail Akdim <1064259@hr.nl>
2023-05-29 08:19:26 +0200

    changed responses

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from database import Account, User, session
 
 app = Flask(__name__)
@@ -18,16 +18,22 @@ def balance():
             if user and user.verify_pin(pin):
                 balance = account.balance
                 response = {'status': 'success', 'balance': balance}
+                # Change the response code to 201 (Created)
+                return make_response(jsonify(response), 201)
             else:
                 response = {'status': 'error', 'message': 'Invalid user ID or PIN.'}
+                # Change the response code to 400 (Bad Request)
+                return make_response(jsonify(response), 400)
         else:
             response = {'status': 'error', 'message': 'Invalid account number.'}
+            # Change the response code to 400 (Bad Request)
+            return make_response(jsonify(response), 400)
 
     except Exception as e:
         print(f"Database Error: {str(e)}")
         response = {'status': 'error', 'message': 'An error occurred while accessing the database.'}
-
-    return jsonify(response)
+        # Change the response code to 500 (Internal Server Error)
+        return make_response(jsonify(response), 500)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
