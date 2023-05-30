6bbc6063d67a513546b438392a1b922eb7e46ac0
Ismail Akdim <1064259@hr.nl>
2023-05-30 11:03:05 +0200

    changed API logic

--- API/main.py
+++ API/main.py
@@ -10,19 +10,19 @@ def balance():
 
     account = session.query(Account).filter_by(iban=iban).first()
     if not account:
-        return jsonify({'status': 400, 'message': 'Invalid account number.'})
+        return jsonify({'status': 400, 'message': 'Invalid account number.', 'success' : False, 'acctNo' : 0})
 
     if account.status == 1:
-        return jsonify({'status': 403, 'message': 'This account is blocked.'})
+        return jsonify({'status': 403, 'message': 'This account is blocked.', 'success' : False})
 
-    user = session.query(User).filter_by(user_status=account.user_id).first()
+    user = session.query(User).filter_by(user_id=account.user_id).first()
     if not user or not user.verify_pin(pin):
         account.increment_attempts()
-        return jsonify({'status': 401, 'message': 'Invalid user status or PIN.'})
+        return jsonify({'status': 401, 'message': 'Invalid user status or PIN.', 'success' : False})
 
     account.reset_attempts()
     balance = account.balance
-    return jsonify({'status': 200, 'balance': balance})
+    return jsonify({'status': 200, 'balance': balance, 'success' : True})
 
 @app.route('/api/withdraw', methods=['POST'])
 def withdraw():
@@ -33,24 +33,24 @@ def withdraw():
 
     account = session.query(Account).filter_by(iban=iban).first()
     if not account:
-        return jsonify({'status': 400, 'message': 'Invalid account number.'})
+        return jsonify({'status': 400, 'message': 'Invalid account number.', 'success' : False})
 
     if account.status == 1:
-        return jsonify({'status': 403, 'message': 'This account is blocked.'})
+        return jsonify({'status': 403, 'message': 'This account is blocked.', 'success' : False})
 
-    user = session.query(User).filter_by(user_status=account.user_id).first()
+    user = session.query(User).filter_by(user_id=account.user_id).first()
     if not user or not user.verify_pin(pin):
         account.increment_attempts()
-        return jsonify({'status': 401, 'message': 'Invalid user status or PIN.'})
+        return jsonify({'status': 401, 'message': 'Invalid user status or PIN.', 'success' : False})
 
     if account.balance < amount:
         account.reset_attempts()
-        return jsonify({'status': 402, 'message': 'Insufficient balance.'})
+        return jsonify({'status': 402, 'message': 'Insufficient balance.', 'success' : False})
 
     account.balance -= amount
     session.commit()
     account.reset_attempts()
-    return jsonify({'status': 200, 'message': 'Withdrawal successful.'})
+    return jsonify({'status': 200, 'message': 'Withdrawal successful.', 'success' : True})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
