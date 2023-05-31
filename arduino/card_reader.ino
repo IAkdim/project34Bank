c4e003476eb675093dd871ef533919c3ae50bd5e
Ismail Akdim <1064259@hr.nl>
2023-05-31 10:43:37 +0200

    added changes from debugging session, added additional errors and fixed some dependencies not being listed in the requirements

--- arduino/card_reader.ino
+++ arduino/card_reader.ino
@@ -8,6 +8,7 @@
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 
+void readAllCardBlocks();
 void setup() {
   Serial.begin(9600);   // Initialize serial communications with the PC
   SPI.begin();      // Init SPI bus
@@ -21,10 +22,10 @@ void loop() {
 
   if (!mfrc522.PICC_ReadCardSerial()) {
     return;
-  
-}
-//  writeBlock(8, "CHBAHE312843209");
-  readCardBlock(8);
+  }
+  // writeBlock(8, "CHBAHE464517766");
+  //    readCardBlock(8);
+  readAll();
 }
 
 void writeBlock(byte blockNumber, String text) {
@@ -84,6 +85,41 @@ void readCardBlock(byte blockNumber) {
   String blockContent = String((char *)buffer);
   Serial.println(blockContent);
 
+  mfrc522.PICC_HaltA();
+  mfrc522.PCD_StopCrypto1();
+}
+
+
+void readAll() {
+  byte buffer[18];
+  byte size = sizeof(buffer);
+  MFRC522::StatusCode status;
+
+  MFRC522::MIFARE_Key key;
+  for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
+
+  for (int i = 0; i < 64; i++) {
+     status = (MFRC522::StatusCode)mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, i, &key, &(mfrc522.uid));
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("PCD_Authenticate() failed: "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+
+  status = (MFRC522::StatusCode)mfrc522.MIFARE_Read(i, buffer, &size);
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("MIFARE_Read() failed: "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+
+  buffer[16] = '\0';
+  
+  String blockContent = String((char *)buffer);
+  Serial.print(blockContent);
+  Serial.print("~");
+  }
+  Serial.println();
   mfrc522.PICC_HaltA();
   mfrc522.PCD_StopCrypto1();
 }
